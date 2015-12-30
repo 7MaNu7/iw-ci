ce0750f73fb9353b0b7338bc066423a3816d4a57
EncarnaAmoros <encaramorosb@gmail.com>
2015-12-30 17:26:14 +0100

    Todos los inputs y en columnas con Bootstrap y CSS

--- Youtube/application/views/youtube/subirvideo.php
+++ Youtube/application/views/youtube/subirvideo.php
@@ -5,78 +5,119 @@
 <main class="container">
 	<h2><?php echo $titulo; ?></h2>
 	
-	<form method="post" accept-charset="utf-8" action="subirvideo/insertar_video" class="formulariosubirvideo"/>
-	
-	<?php
-	
-	$this->load->helper('form');
-	
-	/* Atributos del formulario */	
-	$title = array(
-		'name'        => 'title',
-		'id'          => 'title',
-		'value'       => '',
-		'maxlength'   => '255',
-		'class'				=> 'form-control',
-		'placeholder'	=> 'Ej: Recopilación de vídeos graciosos'
-	);
-	$url = array(
-		'name'        => 'url',
-		'id'          => 'url',
-		'value'       => '',
-		'maxlength'   => '255',
-		'class'				=> 'form-control',
-		'placeholder'	=> 'Ej: https://www.youtube.com/watch?v=p87gfVHMms'
-	);
-	$description = array(
-		'name'        => 'url',
-		'id'          => 'url',
-		'value'       => '',
-		'class'				=> 'form-control formsubirvideotextarea',
-	);
- 	$submit = array(
-			'name' => 'submit',
-			'id' => 'submit',
-			'value' => 'Subir video',
-			'title' => 'Subir video',
-			'class'	=>	'btn btn-primary'
-	);
-	
-	echo '<select name="visibility" class="form-control formsubirvideoselect">';
-	foreach($videovisibilidades as $visibilidad)
-		echo '<option value="' .  $visibilidad->id . '">' . $visibilidad->name . '</option>';
-	echo '</select>'; echo '<br>';
-	
-	echo '<select name="license" class="form-control formsubirvideoselect">';
-	foreach($licenses as $license)
-		echo '<option value="' .  $license->id . '">' . $license->name . '</option>';
-	echo '</select>'; echo '<br>';
-	
-	echo '<select name="category" class="form-control formsubirvideoselect">';
-	foreach($categories as $category)
-		echo '<option value="' .  $category->id . '">' . $category->name . '</option>';
-	echo '</select>'; echo '<br>';
-	
-	echo '<label class="">Título:</label>';
-	echo form_input($title); echo '<br>';
-	echo '<label class="">URL del video:</label>';
-	echo form_input($url); echo '<br>';
-	echo '<label class="">Descripción del video:</label>';
-	echo form_textarea($description); echo '<br>';	
-	
-	echo form_submit($submit);
-	
-	?>
+	<form method="post" accept-charset="utf-8" action="subirvideo/insertar_video" class="row formulariosubirvideo"/>
+		<div class="col-md-6">
+			<?php	
+			$this->load->helper('form');
+			/* Atributos del formulario */	
+			$title = array(
+				'name'        => 'title',
+				'id'          => 'title',
+				'value'       => '',
+				'maxlength'   => '255',
+				'class'				=> 'form-control',
+				'placeholder'	=> 'Ej: Recopilación de vídeos graciosos'
+			);
+			$url = array(
+				'name'        => 'url',
+				'id'          => 'url',
+				'value'       => '',
+				'maxlength'   => '255',
+				'class'				=> 'form-control',
+				'placeholder'	=> 'Ej: https://www.youtube.com/watch?v=p87gfVHMms'
+			);
+			$description = array(
+				'name'        => 'url',
+				'id'          => 'url',
+				'value'       => '',
+				'class'				=> 'form-control formsubirvideotextarea',
+			);
+			$submit = array(
+					'name' => 'submit',
+					'id' => 'submit',
+					'value' => 'Subir video',
+					'title' => 'Subir video',
+					'class'	=>	'btn btn-primary'
+			);
+			?>
+			
+			<label class="">Título:</label>
+			<?php echo form_input($title); echo '<br>'; ?>
+
+			<label class="">URL del video:</label>
+			<?php echo form_input($url); echo '<br>'; ?>
+
+			<label class="">Descripción del video:</label>
+			<?php echo form_textarea($description); echo '<br>';
+			
+			echo form_submit($submit);?>
+		</div>
+
+		<div class="col-md-6">
+			
+			<div class="row formsubirvideodosinputs">
+				<div class="col-md-6 inputpeque">
+					<label class="">Visibilidad del video:</label>
+					<select name="visibility" class="form-control">
+					<?php
+					foreach($videovisibilidades as $visibilidad)
+						echo '<option value="' .  $visibilidad->id . '">' . $visibilidad->name . '</option>';
+					?>
+					</select><br>	
+				</div>
+				<div class="col-md-6 inputpeque">
+					<label class="">Tipo de licencia:</label>
+					<select name="license" class="form-control">
+					<?php
+					foreach($licenses as $license)
+						echo '<option value="' .  $license->id . '">' . $license->name . '</option>';
+					?>
+					</select><br>
+				</div>
+			</div>
+			
+			<label class="">Categoria:</label>
+			<select name="category" class="form-control formsubirvideoselect">
+			<?php
+			foreach($categories as $category)
+				echo '<option value="' .  $category->id . '">' . $category->name . '</option>';
+			?>
+			</select><br>
+
+			<div class="row formsubirvideodosinputs">
+				<div class="col-md-6 inputpeque">
+					<label class="">Idiomas:</label>
+					<select name="languages" class="form-control" multiple>
+					<?php	
+					foreach($languages as $language)
+						echo '<option value="' .  $language->id . '">' . $language->name . '</option>';
+					?>
+					</select><br>
+				</div>
+				<div class="col-md-6 inputpeque">
+					<label class="">Calidades del video:</label>
+					<select name="quality" class="form-control" multiple>
+					<?php
+					foreach($qualities as $quality)
+						echo '<option value="' .  $quality->id . '">' . $quality->name . '</option>';
+					?>
+					</select><br>
+				</div>
+			</div>
+
+			<label class="">Etiquetas:</label>
+			
+		</div>
 	</form>
 	
-<div class="alert alert-danger mensajesSubirVideo" id="mensajeSubirVideo"><?php echo validation_errors();?></div>
+	<div class="alert alert-danger mensajesSubirVideo" id="mensajeSubirVideo"><?php echo validation_errors();?></div>
 
-<script type="text/javascript">
-	var diverrores = document.getElementById('mensajeSubirVideo').innerHTML;
-	if(diverrores=="") {
-		document.getElementById('mensajeSubirVideo').style.display = "none";
-	}
-</script>
+	<script type="text/javascript">
+		var diverrores = document.getElementById('mensajeSubirVideo').innerHTML;
+		if(diverrores=="") {
+			document.getElementById('mensajeSubirVideo').style.display = "none";
+		}
+	</script>
 	
 </main>
 
