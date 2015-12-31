77cdc3d1456c187a302a1f30f322719564032875
EncarnaAmoros <encaramorosb@gmail.com>
2015-12-31 15:12:47 +0100

    Empezando la pantalla de busqueda a partir de la de inicio

---
+++ Youtube/application/models/busqueda_m.php
@@ -0,0 +1,15 @@
+<?php
+
+class Busqueda_m extends CI_Model {
+	
+	function get_all() {
+		$this->db->select('id, title, url, description, duration, user');
+		$this->db->order_by('visits');
+		$this->db->limit(15, 0);
+		return $this->db->get("video")->result();
+	}
+	
+	function count_all() {
+		return $this->db->count_all("video");
+	}
+}
