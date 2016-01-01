85be59ee08ecc201d9b89bb7662b3f0321ef17eb
Jesús Gallego Irles <jesusgallego@outlook.com>
2016-01-01 21:51:10 +0100

    Cambios visuales
    consistencia en las secciones

--- Youtube/application/views/youtube/video.php
+++ Youtube/application/views/youtube/video.php
@@ -3,37 +3,46 @@
 ?>
 
 <main class="container">
-	<section class="col-md-8">
-        <div class="row">
-			<iframe class="video"
-							src="https://www.youtube.com/embed/<?=substr($video->url, 32, 30); ?>"
-							allowfullscreen></iframe>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="col-sm-6">
-                    <div class="row"><h2 class="video-title"><?=$video->title?></h2></div>
-                    <div class="row"><em class="date">Publicado el 29/12/2015</em><span class="user"> By <a href="<?=site_url('canal/ver/' . $video->userid)?>"><?=$video->username?></a></span></div>
-                </div>
-                <div class="col-sm-6 right margin-top">
-                    <div class="row"><?=$video->visits?> Visualizaciones</div>
-                    <div class="row">
-						<span style="margin:5px;color:green"><?=$video->likes?></span> <span class="glyphicon glyphicon-thumbs-up" style="color:green"></span>
-						<span style="margin:5px;color:red"><?=$video->dislikes?></span> <span class="glyphicon glyphicon-thumbs-down" style="color:red"></span>
+	<div class="col-md-8">
+        <section>
+        	<iframe class="video" src="https://www.youtube.com/embed/<?=substr($video->url, 32, 30); ?>" allowfullscreen></iframe>
+        	<div class="description">
+	            <div class="row">
+					<div class="col-sm-6">
+						<h2 class="video-title"><?=$video->title?></h2>
 					</div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
+					<div class="col-sm-6 right margin-top">
+        	            <div class="row">
+							<div class="col-sm-12"><?=$video->visits?> Visualizaciones</div>
+						</div>
+        	            <div class="row">
+							<div class="col-sm-12">
+								<span style="margin:5px;color:green"><?=$video->likes?></span> <span class="glyphicon glyphicon-thumbs-up" style="color:green"></span>
+								<span style="margin:5px;color:red"><?=$video->dislikes?></span> <span class="glyphicon glyphicon-thumbs-down" style="color:red"></span>
+							</div>
+						</div>
+        	        </div>
+				</div>
+	            <div class="row">
+					<div class="col-sm-4">
+						<em class="date">Publicado el 29/12/2015</em>
+					</div>
+					<div class="col-sm-4">
+						<span class="user"> By <a href="<?=site_url('canal/ver/' . $video->userid)?>"><?=$video->username?></a></span>
+					</div>
+				</div>
+				<div class="row">
+	        	    <div class="col-sm-12">
+	        	        <?=$video->description?>
+	        	    </div>
+	        	</div>
+    	    </div>
+        </section>
+        <section>
             <div class="col-sm-12">
-                <?=$video->description?>
+                <h4>Comentarios</h4>
+				<hr>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <h2>Comentarios</h2>
-            </div>
-            <hr class="divider">
 			<?php foreach($comentarios as $comentario) { ?>
 	            <div class="row margin-bottom">
 	                <div class="col-sm-12">
@@ -52,29 +61,30 @@
 	                </div>
 	            </div>
 			<?php } ?>
-        </div>
-    </section>
-	<div class="col-md-1"></div>
-	<section class="col-md-3">
-        <div class="row">
-            <div class="col-sm-12">
-                <h2>Relacionados</h2>
-            </div>
-            <hr class="divider">
-        </div>
-		<?php foreach($related as $rel) { ?>
-	        <div class="row margin-bottom">
-	            <div class="col-sm-12">
-	                <div class="col-sm-4 video-thumb"></div>
-	                <div class="col-sm-8 video-thumb-info">
-	                    <div class="row"><span class="video-thumb-title"><?=$rel->title?></span></div>
-	                    <div class="row"><span class="video-thumb-user"><?=$rel->user?></span></div>
-	                    <div class="row"><span class="video-thumb-views"><?=$rel->visits?> Visualizaciones</span></div>
-	                </div>
-	            </div>
-	        </div>
-		<?php } ?>
-    </section>
+        </section>
+    </div>
+	<div class="col-md-3">
+        <section class="col-md-12">
+			<div class="row">
+        	    <div class="col-sm-12">
+        	        <h4>Relacionados</h4>
+					<hr>
+        	    </div>
+        	</div>
+			<?php foreach($related as $rel) { ?>
+		        <div class="row margin-bottom">
+		            <div class="col-sm-12">
+		                <div class="col-sm-4 video-thumb"></div>
+		                <div class="col-sm-8 video-thumb-info">
+		                    <div class="row"><span class="video-thumb-title"><?=$rel->title?></span></div>
+		                    <div class="row"><span class="video-thumb-user"><?=$rel->user?></span></div>
+		                    <div class="row"><span class="video-thumb-views"><?=$rel->visits?> Visualizaciones</span></div>
+		                </div>
+		            </div>
+		        </div>
+			<?php } ?>
+		</section>
+    </div>
 </main>
 
 <?php
