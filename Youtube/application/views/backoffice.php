db41ef166bae324ae0fe8763401ac229a022cc29
EncarnaAmoros <encaramorosb@gmail.com>
2016-01-02 00:59:06 +0100

    Texto en página principal y CSS en todas las pantallas

--- Youtube/application/views/backoffice.php
+++ Youtube/application/views/backoffice.php
@@ -12,37 +12,40 @@
 	<?php foreach($js_files as $file): ?>
 		<script src="<?php echo $file; ?>"></script>
 	<?php endforeach; ?>
-
-	<style type='text/css'>
-	body
-	{
-		font-family: Arial;
-		font-size: 14px;
-	}
-	a {
-			color: blue;
-			text-decoration: none;
-			font-size: 14px;
-	}
-	a:hover
-	{
-		text-decoration: underline;
-	}
-	</style>
 	
 	<main class="container">
 	
-		<div>
-			<a href='<?php echo site_url('backoffice/gestion_licencias')?>'>Licencias</a>
-			<a href='<?php echo site_url('backoffice/gestion_categorias')?>'>Categorias</a>
-			<a href='<?php echo site_url('backoffice/gestion_visibilidad')?>'>Visibilidad</a>
-			<a href='<?php echo site_url('backoffice/gestion_etiquetas')?>'>Etiquetas</a>
-			<a href='<?php echo site_url('backoffice/gestion_idiomas')?>'>Idiomas</a>
-			<a href='<?php echo site_url('backoffice/gestion_calidades')?>'>Calidades</a>		
-		</div>
+		<?php
+			if(isset($titulo)) {
+				echo "<h1>Gestión/Back-office de Youtube</h1>";
+				echo "<h4 class='backofficetexto'>Eliga la tabla que desee añadir un elemento, verlo, editarlo o eliminarlo:</h4>";
+				
+			?>
+				<div class="enlacesgrocery1">
+					<a href='<?php echo site_url('backoffice/gestion_licencias')?>'>Licencias</a><br>
+					<a href='<?php echo site_url('backoffice/gestion_categorias')?>'>Categorias</a><br>
+					<a href='<?php echo site_url('backoffice/gestion_visibilidad')?>'>Visibilidad</a><br>
+					<a href='<?php echo site_url('backoffice/gestion_etiquetas')?>'>Etiquetas</a><br>
+					<a href='<?php echo site_url('backoffice/gestion_idiomas')?>'>Idiomas</a><br>
+					<a href='<?php echo site_url('backoffice/gestion_calidades')?>'>Calidades</a><br>		
+				</div>
+		<?php	
+			}
+		else {
+		?>		
+			<div class="enlacesgrocery2">
+				<a href='<?php echo site_url('backoffice/gestion_licencias')?>'>Licencias</a>
+				<a href='<?php echo site_url('backoffice/gestion_categorias')?>'>Categorias</a>
+				<a href='<?php echo site_url('backoffice/gestion_visibilidad')?>'>Visibilidad</a>
+				<a href='<?php echo site_url('backoffice/gestion_etiquetas')?>'>Etiquetas</a>
+				<a href='<?php echo site_url('backoffice/gestion_idiomas')?>'>Idiomas</a>
+				<a href='<?php echo site_url('backoffice/gestion_calidades')?>'>Calidades</a>		
+			</div>
+		<?php	
+		}
+		?>
 		
-		<div style='height:20px;'></div>  
-		<div>
+		<div class="tablasgrocery">
 			<?php echo $output; ?>
 		</div>
 		
