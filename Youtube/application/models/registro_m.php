ba8a2eba131c8049a4b20e721f073a043bdaa230
Manuel Verdu <manugustalol@gmail.com>
2016-01-01 01:41:26 +0100

    Usuario se guarda en bd al registrar
    Faltan validaciones y controlar el refrescado posterior

--- Youtube/application/models/registro_m.php
+++ Youtube/application/models/registro_m.php
@@ -2,4 +2,20 @@
 
 class registro_m extends CI_Model {
     
+
+
+	function nueva_cuenta($userName, $email, $password) {
+		$data = array(
+			'userName'	=> $userName,
+			'email' => $email,
+			'password' => $password
+		);
+		
+		//Lo insertamos en la BD
+		$this->db->insert('user',$data);
+		
+		$insert_id = $this->db->insert_id();
+		return  $insert_id;
+		
+	}
 }
