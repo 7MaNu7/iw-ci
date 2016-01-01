081b1929cfca45c19d0a6a7389afa914dab330be
Manuel Verdu <manugustalol@gmail.com>
2016-01-01 06:02:32 +0100

    Añadidas validaciones al registro
    Entre ellas tenemos validar un patron para el email: **@**.**

--- Youtube/application/controllers/registro.php
+++ Youtube/application/controllers/registro.php
@@ -11,23 +11,86 @@ class registro extends CI_Controller {
     //Por defecto, si no ay index error
     public function index()
     {
+        $data['usuarios']=$this->registro_m->get_all();
+        $data['cuantos']=$this->registro_m->count_all();
         $data['css_files'] = ["assets/css/cabecera.css", "assets/css/subirvideo.css"];
         $data['js_files'] = ["assets/js/cabecera.js"];
         $data['titulo']="Registrarse";
         $this->load->view('youtube/registro', $data);
     }
+
+    public function probando($email)
+    {
+
+        $fracciones = explode("@", $email);
+                
+        if (sizeof($fracciones)!=2)
+        {
+            $this->form_validation->set_message('probando', 'El campo %s debe seguir el formato ***@***.***');
+            return FALSE;
+        }
+        else
+        {
+            $cadFinal = explode(".", $fracciones[1]);
+            if(sizeof($cadFinal)!=2)
+            {
+                $this->form_validation->set_message('probando', 'El campo %s debe seguir el formato ***@***.***');
+                return FALSE;
+            }
+            else if(strlen($cadFinal[1])<2 || strlen($cadFinal[0])<2)
+            {
+                $this->form_validation->set_message('probando', 'El campo %s debe tener minimo dos caracteres despues del "@" y del "."');
+                return FALSE; 
+            }
+            return TRUE;
+        }
+    }
     
     public function insertar_usuario(){
 
-        $userName = $this->input->post('userName');   
-        $email = $this->input->post('email');       
-        $password = $this->input->post('password');   
 
-        //ahora procesamos los datos hacía el modelo que debemos crear
-        $nuevo_usuario = $this->registro_m->nueva_cuenta(
-            $userName,
-            $email,
-            $password
-        );
+        //si se ha pulsado el botón submit validamos el formulario con codeIgniter
+        if($this->input->post('submit')) {
+            //hacemos las comprobaciones que deseemos en nuestro formulario
+            $this->form_validation->set_rules('userName','userName','trim|required|xss_clean|is_unique[user.userName]');
+            //$this->form_validation->set_rules('email','email','trim|required|xss_clean|is_unique[user.email]');
+            $this->form_validation->set_rules('password','password','trim|required|xss_clean');
+            $this->form_validation->set_rules('email','email','trim|required|xss_clean|callback_probando');
+/*
+            $usuarios = $this->registro_m->get_all();
+            foreach ($usuarios as $user) {
+                if($user.email=="caca")
+                {
+
+                }
+            }
+*/
+            //validamos que se introduzcan los campos requeridos con la función de ci required
+            $this->form_validation->set_message('required', 'El campo %s es obligatorio');
+
+            if (!$this->form_validation->run())
+            {
+                //si no pasamos la validación volvemos al formulario mostrando los errores
+                $this->index();
+            }
+            //si pasamos la validación correctamente pasamos a hacer la inserción en la base de datos
+            else {
+
+                $userName = $this->input->post('userName');   
+                $email = $this->input->post('email');       
+                $password = $this->input->post('password');   
+
+                //ahora procesamos los datos hacía el modelo que debemos crear
+                $nuevo_usuario = $this->registro_m->nueva_cuenta(
+                    $userName,
+                    $email,
+                    $password
+                );
+
+                $this->load->helper('url');
+                redirect("login", 'refresh');
+
+            }
+        }
     }
 }
