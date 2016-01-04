3651621c94bb157b1cdabd9861b28478fec207d0
EncarnaAmoros <encaramorosb@gmail.com>
2016-01-04 16:13:14 +0100

    Email y password de login

--- Youtube/application/views/youtube/login.php
+++ Youtube/application/views/youtube/login.php
@@ -9,7 +9,7 @@
 		<label>Email:</label>
 		<input type="text" name="email" id="email" class="form-control"> </input>
 		<label>Password:</label>
-		<input type="password" name="password" class="form-control"> </input>
+		<input type="password" name="password" id="password" class="form-control"> </input>
 	 	<input class="btn btn-primary botonlogin" value="Iniciar sesión" type="submit" name="submit"/>
 	</form>
 
--- Youtube/assets/js/validacion-formularios.js
+++ Youtube/assets/js/validacion-formularios.js
@@ -1,9 +1,18 @@
+/* LOGIN */
 var email = document.getElementById("email");
 email.val;
 
 email.onblur = function() {
 	if(email.value == "") {
 		email.style.borderColor = "rgba(255, 0, 0, 0.51)";
-		email.appendChild('<h1>hola</h1>')
+	}
+}
+
+var password = document.getElementById("password");
+password.val;
+
+email.onblur = function() {
+	if(email.value == "") {
+		email.style.borderColor = "rgba(255, 0, 0, 0.51)";
 	}
 }
