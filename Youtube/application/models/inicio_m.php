c0c27749b9da40d8957e4bf3e0ef4937523ee32d
Jesús Gallego Irles <jesusgallego@outlook.com>
2016-01-04 19:32:30 +0100

    Videos en inicio
    El orden de los vídeos de inicio ahora es descendente
    con respecto a las visitas

--- Youtube/application/models/inicio_m.php
+++ Youtube/application/models/inicio_m.php
@@ -3,7 +3,7 @@
 class Inicio_m extends CI_Model {
 
 	function get_all() {
-		$query = $this->db->query('SELECT v.id, v.title, v.url, v.description, u.username, u.id userid FROM video v, user u WHERE v.user=u.id ORDER BY v.visits LIMIT 21');
+		$query = $this->db->query('SELECT v.id, v.title, v.url, v.description, u.username, u.id userid FROM video v, user u WHERE v.user=u.id ORDER BY v.visits DESC LIMIT 21');
 		return $query->result();
 	}
 
