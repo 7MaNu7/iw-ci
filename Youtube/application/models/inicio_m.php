286920e56af846039b32983e4045e2882bfc9b10
EncarnaAmoros <encaramorosb@gmail.com>
2016-01-04 19:02:09 +0100

    Limit 21 a los videos que se muestran en la pantalla principal

--- Youtube/application/models/inicio_m.php
+++ Youtube/application/models/inicio_m.php
@@ -3,10 +3,7 @@
 class Inicio_m extends CI_Model {
 
 	function get_all() {
-		$this->db->select('id, title, url, description, duration, user');
-		$this->db->order_by('visits');
-		$this->db->limit(15, 0);
-		$query = $this->db->query('SELECT v.id, v.title, v.url, v.description, u.username, u.id userid FROM video v, user u WHERE v.user=u.id ORDER BY v.visits');
+		$query = $this->db->query('SELECT v.id, v.title, v.url, v.description, u.username, u.id userid FROM video v, user u WHERE v.user=u.id ORDER BY v.visits LIMIT 21');
 		return $query->result();
 	}
 
