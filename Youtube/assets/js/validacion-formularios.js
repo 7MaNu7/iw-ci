2ff58d89a335e322d7cdad5a72ec5e54a6b7f422
EncarnaAmoros <encaramorosb@gmail.com>
2016-01-04 16:22:42 +0100

    Vuelven a gris cuando los campos tienen algo escrito

--- Youtube/assets/js/validacion-formularios.js
+++ Youtube/assets/js/validacion-formularios.js
@@ -5,14 +5,18 @@ email.val;
 email.onblur = function() {
 	if(email.value == "") {
 		email.style.borderColor = "rgba(255, 0, 0, 0.51)";
+	} else {
+		email.style.borderColor = "#ccc";
 	}
 }
 
 var password = document.getElementById("password");
 password.val;
 
-email.onblur = function() {
-	if(email.value == "") {
-		email.style.borderColor = "rgba(255, 0, 0, 0.51)";
+password.onblur = function() {
+	if(password.value == "") {
+		password.style.borderColor = "rgba(255, 0, 0, 0.51)";
+	} else {
+		password.style.borderColor = "#ccc";
 	}
 }
