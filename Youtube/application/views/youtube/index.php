8027e4a4031e53f303cffe38f476dd955a633882
EncarnaAmoros <encaramorosb@gmail.com>
2016-01-05 16:18:59 +0100

    Mensaje log out con transición CSS

--- Youtube/application/views/youtube/index.php
+++ Youtube/application/views/youtube/index.php
@@ -5,19 +5,20 @@
 
 <main class="container">
 
-	<div id="alerta"></div>
+	<!--<div id="alerta"></div>-->
+	<!--Mensaje cierre de sesión -->
+	<div id="divmensajelogin" style="width:500px; margin-left: 250px; margin-top: 15px;" class="mensajeoculto">
+		<div class="alert alert-success" id="mensajelogin">Cierre de sesión correcto</div>
+	</div>
+	
 	<h2><?=$titulo?></h2>
 
 	<div class="videospopulares">
 		<?php
-
 			if(sizeof($videos)!=0)
 			{
 				foreach($videos as $video) { 
-			
-			
 		?>
-
 				<div class="bloquevideoinicio">
 					<a href="<?=site_url('/video/watch/' . $video->id)?>">
 						<h5> <?=$video->title?></h5>
@@ -27,13 +28,9 @@
 						<img src="http://img.youtube.com/vi/<?php echo substr($video->url, 32, 30); ?>/0.jpg" alt="" class="videoinicio"/>
 					</a>
 				</div>
-
 		<?php 
 				}
-
-			}
-			else
-			{
+			} else {
 		?>
 				<h4>Todavía no hay ningún video subido en la web</h4>
 				<h5>¡Sé el primero en <a href="<?=site_url('/subirvideo')?>">subir un video</a>!</h5>
@@ -43,16 +40,18 @@
 	</div>
 
     <script type="text/javascript">
-
-    	var funcion = function() {
+    	/*var funcion = function() {
     		document.getElementById("alerta").innerHTML='<div class="alert alert-success errorlogin">Cerrado de sesión correcto</div>';
     		setTimeout(function(){window.location="logout"}, 2000);
-    		//href="<?=site_url('logout')?>"
-    	}
-
+    	}*/
+			var funcion = function() {
+				var mensaje = document.getElementById("mensajelogin");
+				var divmensaje = document.getElementById("divmensajelogin");
+				mensaje.className = "alert alert-success";
+				divmensaje.className = "mensajevisible";
+				setTimeout(function(){window.location="logout"}, 2000);
+			}
     	document.getElementById("salir").onclick=funcion;
-
-
     </script>
 
 </main>
