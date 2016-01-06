6006f432e7a61ee1e043a3e9dfc46b96b676a750
Jesús Gallego Irles <jesusgallego@outlook.com>
2016-01-06 23:33:00 +0100

    Fondo editar video y botones

--- Youtube/application/views/youtube/editarvideo.php
+++ Youtube/application/views/youtube/editarvideo.php
@@ -3,264 +3,268 @@
 ?>
 
 <main class="container">
-	<h2><?php echo $titulo; ?></h2>
+	<section class="padding">
+		<h2><?php echo $titulo; ?></h2>
 
-	<?php
+		<?php
 
-		//Si no es el creador del video no tiene acceso a modificarlo
-		if(!isset($_SESSION["id"]) || $_SESSION["id"]!=$video->userid)
-			echo '<script>window.location.href="' . base_url() . 'index.php/video/watch/' . $video->id . '"</script>';
+			//Si no es el creador del video no tiene acceso a modificarlo
+			if(!isset($_SESSION["id"]) || $_SESSION["id"]!=$video->userid)
+				echo '<script>window.location.href="' . base_url() . 'index.php/video/watch/' . $video->id . '"</script>';
 
-	?>
+		?>
 
-	<form method="post" accept-charset="utf-8"
-				action="<?php echo base_url()?>index.php/video/modificar_video" class="row formularioeditarvideo"/>
-		<div class="col-md-6">
+		<form method="post" accept-charset="utf-8"
+					action="<?php echo base_url()?>index.php/video/modificar_video" class="row formularioeditarvideo"/>
+			<div class="col-md-6">
 
 
-			<?php
-			$this->load->helper('form');
-			/* Atributos del formulario */
-			$title = array(
-				'name'        => 'title',
-				'id'          => 'title',
-				'value'       => $video->title,
-				'maxlength'   => '255',
-				'class'				=> 'form-control',
-				'placeholder'	=> 'Ej: Recopilación de vídeos graciosos'
-			);
-			$url = array(
-				'name'        => 'url',
-				'id'          => 'url',
-				'value'       => $video->url,
-				'maxlength'   => '255',
-				'class'				=> 'form-control',
-				'placeholder'	=> 'Ej: https://www.youtube.com/watch?v=p87gfVHMms',
-				'readonly'		=>	'readonly'
-			);
-			$description = array(
-				'name'        => 'description',
-				'id'          => 'description',
-				'value'       => $video->description,
-				'class'				=> 'form-control formeditarvideotextarea',
-				'placeholder'	=> 'Ej: El mejor vídeo de risa que puedas ver. Muestra una serie situaciones graciosas con las que vas a disfrutar...'
-			);
-			$submit = array(
-					'name' => 'submit',
-					'id' => 'submit',
-					'value' => 'Guardar cambios',
-					'title' => 'Guardar cambios',
-					'class'	=>	'btn btn-primary botoneditarvideo'
-			);
-			?>
+				<?php
+				$this->load->helper('form');
+				/* Atributos del formulario */
+				$title = array(
+					'name'        => 'title',
+					'id'          => 'title',
+					'value'       => $video->title,
+					'maxlength'   => '255',
+					'class'				=> 'form-control',
+					'placeholder'	=> 'Ej: Recopilación de vídeos graciosos'
+				);
+				$url = array(
+					'name'        => 'url',
+					'id'          => 'url',
+					'value'       => $video->url,
+					'maxlength'   => '255',
+					'class'				=> 'form-control',
+					'placeholder'	=> 'Ej: https://www.youtube.com/watch?v=p87gfVHMms',
+					'readonly'		=>	'readonly'
+				);
+				$description = array(
+					'name'        => 'description',
+					'id'          => 'description',
+					'value'       => $video->description,
+					'class'				=> 'form-control formeditarvideotextarea',
+					'placeholder'	=> 'Ej: El mejor vídeo de risa que puedas ver. Muestra una serie situaciones graciosas con las que vas a disfrutar...'
+				);
+				$submit = array(
+						'name' => 'submit',
+						'id' => 'submit',
+						'value' => 'Guardar cambios',
+						'title' => 'Guardar cambios',
+						'class'	=>	'btn btn-primary btn-block'
+				);
+				?>
 
-			<label class=""><span class="campoobligatorio">(*) </span>Título:</label>
-			<?php echo form_input($title); echo '<br>'; ?>
+				<label class=""><span class="campoobligatorio">(*) </span>Título:</label>
+				<?php echo form_input($title); echo '<br>'; ?>
 
-			<label class=""><span class="campoobligatorio">(*) </span>URL del video:</label>
-			<?php echo form_input($url); echo '<br>'; ?>
+				<label class=""><span class="campoobligatorio">(*) </span>URL del video:</label>
+				<?php echo form_input($url); echo '<br>'; ?>
 
-			<label class="">Descripción del video:</label>
-			<?php echo form_textarea($description); echo '<br>';?>
+				<label class="">Descripción del video:</label>
+				<?php echo form_textarea($description); echo '<br>';?>
 
-			<p>(*): El campo es obligatorio.</p>
+				<p>(*): El campo es obligatorio.</p>
 
-			<?php $_SESSION["videoId"]= $video->id ?>
+				<?php $_SESSION["videoId"]= $video->id ?>
 
-			<?php echo form_submit($submit);?>
-
-		</div>
-		<div class="col-md-6">
-
-			<div class="row formeditarvideodosinputs">
-				<div class="col-md-6 inputpeque">
-					<label class="">Visibilidad del video:</label>
-
-					<select name="visibility" class="form-control">
-
-					<?php
-					foreach($videovisibilidades as $visibilidad)
-					{
-						if($visibilidad->id == $video->visibility)
-						{
-							echo '<option value="' .  $visibilidad->id . '" selected>' . $visibilidad->name . '</option>';
-						}
-						else
-						{
-							echo '<option value="' .  $visibilidad->id . '">' . $visibilidad->name . '</option>';
-						}
-					}
-					?>
-
-					</select><br>
-				</div>
-				<div class="col-md-6 inputpeque">
-					<label class="">Tipo de licencia:</label>
-					<select name="license" class="form-control">
-
-					<?php
-					foreach($licenses as $license)
-					{
-						if($license->id == $video->license)
-						{
-							echo '<option value="' .  $license->id . '" selected>' . $license->name . '</option>';
-						}
-						else
-						{
-							echo '<option value="' .  $license->id . '">' . $license->name . '</option>';
-						}
-					}
-					?>
-
-					</select><br>
-				</div>
 			</div>
+			<div class="col-md-6">
 
-			<label class="">Categoria:</label>
-			<select name="category" class="form-control formeditarvideoselect">
+				<div class="row formeditarvideodosinputs">
+					<div class="col-md-6 inputpeque">
+						<label class="">Visibilidad del video:</label>
 
-			<?php
-				foreach($categories as $category)
-				{
-					if($category->id == $video->category)
-					{
-						echo '<option value="' .  $category->id . '" selected>' . $category->name . '</option>';
-					}
-					else
-					{
-						echo '<option value="' .  $category->id . '">' . $category->name . '</option>';
-					}
-				}
-			?>
+						<select name="visibility" class="form-control">
 
-			</select><br>
-
-			<div class="row formeditarvideodosinputs">
-				<div class="col-md-6 inputpeque">
-					<label class="">Idiomas:</label>
-					<select name="language" class="form-control">
-
-					<?php
-
-					foreach($languages as $language)
-					{
-						if($language->id == $video->language)
+						<?php
+						foreach($videovisibilidades as $visibilidad)
 						{
-							echo '<option value="' .  $language->id . '" selected>' . $language->name . '</option>';
-						}
-						else
-						{
-							echo '<option value="' .  $language->id . '">' . $language->name . '</option>';
-						}
-					}
-					?>
-
-					</select><br>
-				</div>
-				<div class="col-md-6 inputpeque">
-					<label class="">Calidades del video:</label>
-					<select name="qualities[]" class="form-control" multiple>
-
-					<?php
-
-					$calidades = $videoqualities;
-
-					foreach($qualities as $quality)
-					{
-
-						$encontrado = false;
-						for($i=0; $i<sizeof($calidades); $i++)
-						{
-							echo $calidades[$i]->id;
-							if($quality->id==$calidades[$i]->id)
+							if($visibilidad->id == $video->visibility)
 							{
-								$encontrado = true;
+								echo '<option value="' .  $visibilidad->id . '" selected>' . $visibilidad->name . '</option>';
+							}
+							else
+							{
+								echo '<option value="' .  $visibilidad->id . '">' . $visibilidad->name . '</option>';
 							}
 						}
+						?>
 
-						if($encontrado==true)
+						</select><br>
+					</div>
+					<div class="col-md-6 inputpeque">
+						<label class="">Tipo de licencia:</label>
+						<select name="license" class="form-control">
+
+						<?php
+						foreach($licenses as $license)
 						{
-							echo '<option value="' .  $quality->id . '" selected>' . $quality->name . '</option>';
+							if($license->id == $video->license)
+							{
+								echo '<option value="' .  $license->id . '" selected>' . $license->name . '</option>';
+							}
+							else
+							{
+								echo '<option value="' .  $license->id . '">' . $license->name . '</option>';
+							}
+						}
+						?>
+
+						</select><br>
+					</div>
+				</div>
+
+				<label class="">Categoria:</label>
+				<select name="category" class="form-control formeditarvideoselect">
+
+				<?php
+					foreach($categories as $category)
+					{
+						if($category->id == $video->category)
+						{
+							echo '<option value="' .  $category->id . '" selected>' . $category->name . '</option>';
 						}
 						else
 						{
-							echo '<option value="' .  $quality->id . '">' . $quality->name . '</option>';
+							echo '<option value="' .  $category->id . '">' . $category->name . '</option>';
 						}
 					}
-					?>
-					</select><br>
+				?>
+
+				</select><br>
+
+				<div class="row formeditarvideodosinputs">
+					<div class="col-md-6 inputpeque">
+						<label class="">Idiomas:</label>
+						<select name="language" class="form-control">
+
+						<?php
+
+						foreach($languages as $language)
+						{
+							if($language->id == $video->language)
+							{
+								echo '<option value="' .  $language->id . '" selected>' . $language->name . '</option>';
+							}
+							else
+							{
+								echo '<option value="' .  $language->id . '">' . $language->name . '</option>';
+							}
+						}
+						?>
+
+						</select><br>
+					</div>
+					<div class="col-md-6 inputpeque">
+						<label class="">Calidades del video:</label>
+						<select name="qualities[]" class="form-control" multiple>
+
+						<?php
+
+						$calidades = $videoqualities;
+
+						foreach($qualities as $quality)
+						{
+
+							$encontrado = false;
+							for($i=0; $i<sizeof($calidades); $i++)
+							{
+								echo $calidades[$i]->id;
+								if($quality->id==$calidades[$i]->id)
+								{
+									$encontrado = true;
+								}
+							}
+
+							if($encontrado==true)
+							{
+								echo '<option value="' .  $quality->id . '" selected>' . $quality->name . '</option>';
+							}
+							else
+							{
+								echo '<option value="' .  $quality->id . '">' . $quality->name . '</option>';
+							}
+						}
+						?>
+						</select><br>
+					</div>
+				</div>
+
+				<?php
+
+					$cadenatags="";
+
+					$i=0;
+					foreach($videotags as $vt)
+					{
+						$cadenatags=$cadenatags . $vt->name;
+						$i++;
+
+						if($i<sizeof($videotags))
+						{
+							$cadenatags=$cadenatags . ',';
+						}
+					}
+
+					$etiquetas = array(
+					'name'       	 	=> 'etiquetas',
+					'id'          		=> 'etiquetas',
+					'value'       		=>  $cadenatags,
+					'class'				=> 'form-control formeditarvideotextareasmall',
+					'placeholder'		=> 'Etiquetas (p. ej: Albert Einstein, gatitos, comedia)'
+				);
+
+				?>
+
+				<label class="">Etiquetas:</label>
+				<!--<?php echo form_textarea($etiquetas); echo '<br>';?>-->
+				<select name="etiquetas[]" id="tag-select" class="form-control" multiple>
+				</select>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="col-md-3 col-md-offset-6 margin-top">
+						<a class="btn btn-danger btn-borrar" data-toggle="modal" data-target="#delete-modal" data-user="<?=$video->userid?>" data-video="<?=$video->id?>" data-title="<?=$video->title?>"><i class="glyphicon glyphicon-trash"></i> Borra este video</a>
+					</div>
+					<div class="col-md-3 margin-top">
+						<?php echo form_submit($submit);?>
+					</div>
 				</div>
 			</div>
-
+		</form>
+		<script type="text/javascript">
 			<?php
+			$js_array = json_encode($tags);
+			echo "var javascript_array = ". $js_array . ";";
+			 ?>
+			 var ts = $('#tag-select');
+			ts.select2({
+				data: javascript_array,
+				tags: true,
+				placeholder: 'Etiquetas (p. ej: Albert Einstein, gatitos, comedia)',
+				tokenSeparators: [',']
+			});
+			<?php foreach ($videotags as $etiqueta) { ?>
+				var opt = $('<option value="<?=$etiqueta->name?>" selected="selected"><?=$etiqueta->name?></option>');
+				ts.append(opt).trigger('change');
+			<?php } ?>
 
-				$cadenatags="";
+		</script>
 
-				$i=0;
-				foreach($videotags as $vt)
-				{
-					$cadenatags=$cadenatags . $vt->name;
-					$i++;
+		<div class="alert alert-danger mensajesEditarVideo" id="mensajeEditarVideo"><?php echo validation_errors();?></div>
 
-					if($i<sizeof($videotags))
-					{
-						$cadenatags=$cadenatags . ',';
-					}
-				}
-
-				$etiquetas = array(
-				'name'       	 	=> 'etiquetas',
-				'id'          		=> 'etiquetas',
-				'value'       		=>  $cadenatags,
-				'class'				=> 'form-control formeditarvideotextareasmall',
-				'placeholder'		=> 'Etiquetas (p. ej: Albert Einstein, gatitos, comedia)'
-			);
-
-			?>
-
-			<label class="">Etiquetas:</label>
-			<!--<?php echo form_textarea($etiquetas); echo '<br>';?>-->
-			<select name="etiquetas[]" id="tag-select" class="form-control" multiple>
-			</select>
-		</div>
-	</form>
-	<script type="text/javascript">
 		<?php
-		$js_array = json_encode($tags);
-		echo "var javascript_array = ". $js_array . ";\n";
-		 ?>
-		 var ts = $('#tag-select');
-		ts.select2({
-			data: javascript_array,
-			tags: true,
-			placeholder: 'Etiquetas (p. ej: Albert Einstein, gatitos, comedia)',
-			tokenSeparators: [',']
-		});
-		<?php foreach ($videotags as $etiqueta) { ?>
-			var opt = $('<option value="<?=$etiqueta->name?>" selected="selected"><?=$etiqueta->name?></option>');
-			ts.append(opt).trigger('change');
-		<?php } ?>
-
-	</script>
-		<div class="row">
-			<div class="col-md-6 col-md-offset-6">
-				<button class="btn btn-danger btn-borrar" data-toggle="modal" data-target="#delete-modal" data-user="<?=$video->userid?>" data-video="<?=$video->id?>" data-title="<?=$video->title?>"><i class="glyphicon glyphicon-trash"></i> Borra este video</button>
-			</div>
-		</div>
-
-	<div class="alert alert-danger mensajesEditarVideo" id="mensajeEditarVideo"><?php echo validation_errors();?></div>
-
-	<?php
-	if(isset($_POST["submit"])){
-		if (session_status() == PHP_SESSION_NONE)
-			session_start();
-		if (!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
-			$response = "Debes iniciar sesión.";
-			echo '<div class="alert alert-danger mensajesEditarVideo">Error: '.$response.'</div>';
+		if(isset($_POST["submit"])){
+			if (session_status() == PHP_SESSION_NONE)
+				session_start();
+			if (!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
+				$response = "Debes iniciar sesión.";
+				echo '<div class="alert alert-danger mensajesEditarVideo">Error: '.$response.'</div>';
+			}
 		}
-	}
-	?>
-
+		?>
+	</section>
 </main>
 
 <!-- Modal -->
